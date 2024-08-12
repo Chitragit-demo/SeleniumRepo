76ee07dd77c359d244b7d8656d99198fab7b7555
Chitragit-demo <chitra91btech@gmail.com>
2024-08-12 21:12:05 +0530

    day1assignment

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	float browserVersion = 91.0f;
+	String browser = "Google";
+	Boolean isBeta = false;
+	int releaseYear = 2008;
+	char shortcut = 'C';
+	
+	System.out.println("Browser: " +browser);
+	System.out.println("Browser Version: " +browserVersion);
+	System.out.println("isBeta: "+isBeta
+			+"\n"+ "releaseYear:" +releaseYear
+			+"\n"+ "Short Cut:" +shortcut);
+	}
+
+}
