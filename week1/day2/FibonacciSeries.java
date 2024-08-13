701284e9ff6918babf5e18c2140aafc5f4a43d43
Chitragit-demo <chitra91btech@gmail.com>
2024-08-13 21:19:15 +0530

    Update FibonacciSeries.java

--- week1/day2/FibonacciSeries.java
+++ week1/day2/FibonacciSeries.java
@@ -3,8 +3,17 @@ package week1.day2;
 public class FibonacciSeries {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Analysis - Yet to complete
+		int max=8;
+		int a = 0, b = 1, c;
+		System.out.println("Print the fibonacci series");
+		System.out.println(a);
+		System.out.println(b);
+		for (int i=0;i<=max;i++)
+		{
+			c=a+b;
+			System.out.println(c);
+			a=b;
+			b=c;
+		}
 	}
-
 }
