c1a5a00b834254ae54acde09c4ac9f25266ae94d
Chitragit-demo <chitra91btech@gmail.com>
2024-08-13 17:06:10 +0530

    Add files via upload

---
+++ week1/day2/ConditionalStmt.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+public class ConditionalStmt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int mark = 45;
+		if(mark<20) {
+		System.out.println("Grade C");
+		}
+		else if(mark<40) {
+			System.out.println("Grade B");
+		}
+		else if(mark<50) {
+			System.out.println("Grade A");
+		}
+		else {
+			System.out.println("Grade O");
+		}
+	}
+}
